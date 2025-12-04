e8b79d0c905dc48acb5222f2b127a7342cf992c2
samg1010 <samgaspar1010@gmail.com>
2025-12-04 10:59:01 +0800

    REMOVE broken dashboard – final build

--- app/dashboard/UserDashboard.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/UserDashboard.tsx
-'use client';
-
-import { UserButton } from '@clerk/nextjs';
-
-export function UserDashboard() {
-  return <UserButton afterSignOutUrl="/" />;
-}
